2836aec0a3093e81db337ab9d6b763c0ce269699
Nicholas Grimaldi <98732607+nickyg-42@users.noreply.github.com>
2023-01-29 21:52:17 -0500

    more files

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 const searchInput = document.querySelector('.search');
+const input = document.getElementById('input');
 const url = "https://restcountries.com/v3.1/name/";
 
 searchInput.addEventListener("click", () => {
-    let val = document.getElementById("input").value;
+    let val = input.value;
 
     if (val && val.trim().length > 0) {
-        val = val.trim().toLowerCase();
+        val = val
+            .trim()
+            .toLowerCase()
+            .concat("?fields=name,currencies,flags,capital,region,population,languages");
 
         fetch(url.concat(val), {
                 method: 'GET'
             })
             .then((response) => response.json())
             .then(data => {
-                console.log(data);
+                console.log(data.name);
+                localStorage.stuff = JSON.stringify(data);
+                window.location.href = "country.html";
+                
             })
             .catch((error) => {
                 console.log(error);
+                // bad request, display to user with error code and ask to try again
         });
-
-        const page = window.location.href = "country.html";
         
     } else {
-        // invalid. stay on page and give error message
+        // invalid input. stay on page and give error message
     }
 })
+
+input.addEventListener('keydown', (e) => {
+    var regex = new RegExp('[a-zA-Z]');
+
+    if(e.ctrlKey || e.altKey || typeof e.key !== 'string' || e.key.length !== 1) return;
+
+    if (!regex.test(e.key)) {
+        e.preventDefault();
+    }
+}, false);
+
+// name         flag
+// capital language currency region population
